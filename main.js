02158bd43a87ae178ee4e262ccf49465e18da163
Avrora <smoyanavrora2008@gmail.com>
2026-04-17 11:24:44 +0300

    updatee

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import { createOption } from "./currencies.js"
-import { distanceChanger } from "./measures.js"
-import { list__actions } from "./list__actions.js"
-import { clearBtn } from "./list__actions.js"
-import { countBudget } from "./total__budget.js"
+import { createOption } from "currencies.js"
+import { distanceChanger } from "measures.js"
+import { list__actions } from "list__actions.js"
+import { clearBtn } from "list__actions.js"
+import { countBudget } from "total__budget.js"
 
 
 
